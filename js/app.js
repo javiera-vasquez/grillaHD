74b451ed02a7a4a251f8aee3d397b07bf5ad292d
Javier Enrique <s0wkete@gmail.com>
2015-02-11 23:22:16 -0300

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
+// Init foundation
 $(document).foundation();
-$("#nav").sticky();
-$("#region").sticky({
-    topSpacing: 80
+
+// Fixed element's
+$("#nav").scrollToFixed({
+    zIndex: 9999,
 });
+
+// Fixed the region combos
+$("#region").scrollToFixed({
+    marginTop: 80,
+    zIndex: 9900,
+    limit: function() {
+        var limit = $("#hdInfo").offset().top - $(this).outerHeight(true) - 80
+        return limit;
+    }
+});
+
+// Fixed the tabs
+$("#fixedNavChannel").scrollToFixed({
+    marginTop: $('#nav').outerHeight(true) + 60,
+    limit: function() {
+        var limit = $("#hdInfo").offset().top - $(this).outerHeight(true) - 80
+        return limit;
+    }
+});
+
